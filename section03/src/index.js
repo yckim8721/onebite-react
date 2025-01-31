ef90bfb036d33e02c96ff3848ba86145a90834e5
yckim8721 <yckim8721@gmail.com>
2025-01-31 14:34:57 +0900

    섹션4. Node.js 기초

---
+++ section03/src/index.js
@@ -0,0 +1,10 @@
+import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
