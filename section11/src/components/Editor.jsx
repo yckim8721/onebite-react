c7cfa3f58f58ccda4375ab65879a916de4523d59
yckim8721 <yckim8721@gmail.com>
2025-02-04 11:23:03 +0900

    섹션12. Context

---
+++ section11/src/components/Editor.jsx
@@ -0,0 +1,42 @@
+import { useState, useRef, useContext } from "react";
+import "./Editor.css";
+import { TodoDispatchContext } from "../App";
+
+const Editor = () => {
+  const { onCreate } = useContext(TodoDispatchContext);
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
